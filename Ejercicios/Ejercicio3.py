a5f832682313f8e2d36f8cc538eb2efd6e30d36d
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2021-11-28 08:03:38 +0100

    subir

---
+++ Ejercicios/Ejercicio3.py
@@ -0,0 +1,9 @@
+#Definir un max() que tome dos numeros y que devuelva el mayor de ellos
+
+def max (n1, n2):
+    if n1 < n2:
+        print (n2)
+    elif n2 < n1:
+        print (n1)
+    else:
+        print ("Son iguales")
