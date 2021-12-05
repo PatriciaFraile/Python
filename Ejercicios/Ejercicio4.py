c7362cc7362aef94342cd177f76dd1838ca63ddc
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2021-12-05 19:56:08 +0100

    subir

---
+++ Ejercicios/Ejercicio4.py
@@ -0,0 +1,8 @@
+#Crea una variable  numerica y si esta entre 0 y 10 , mostrar un mensaje indicandolo
+
+b = 8
+
+if(b>=1 and b<=10):
+    print("Esta en el rango 1 y 10")
+else:
+    print("No esta en el rango")
