941e2df7b87c21adae61a6a801de8d5328bc3c2a
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2022-05-02 20:24:42 +0200

    subir

--- Ejercicios/Arrays/ejercicio1.py
+++ Ejercicios/Arrays/ejercicio1.py
@@ -1,10 +1,10 @@
 #Crea un array o arreglo unidimensional donde le indiques el tamaño por teclado y 
 #crear una función que rellene el array o arreglo con los múltiplos de un número pedido por teclado.
 
-n = int(input("Ingrese el tamaño del arreglo"))
-m = int(input("Ingrese el número de múltiplos"))
+tamanio = int(input("Ingrese el tamaño del arreglo"))
+numero = int(input("Ingrese el número de múltiplos"))
 A = []
-for i in range (0,n):
- A.append(i*m)
+for i in range (0,tamanio):
+ A.append(i*numero)
 
 print (A)
