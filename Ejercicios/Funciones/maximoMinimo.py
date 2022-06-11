782919fbeef1497f79381ba14dce0c6df909edc4
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2022-06-11 16:30:41 +0200

    subir

---
+++ Ejercicios/Funciones/maximoMinimo.py
@@ -0,0 +1,14 @@
+def maximo (a,b):
+    if a>b:
+        return a 
+    else :
+        return b
+def minimo(a,b):
+    if a <b :
+        return a 
+    else:
+        return b
+a = int(input("Escribe el primer numero"))
+b = int (input("Escribe el segundo numero"))
+print(maximo(a,b))
+print(minimo(a,b))
