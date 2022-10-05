9830cbaf6b8c6063cd43d20e9cad2d3d64be71e9
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2022-10-05 23:11:35 +0200

    subir

---
+++ Diagramas/ejercicioUno.py
@@ -0,0 +1,10 @@
+#Realiza un diagrama de flujo que convierta de millas a kilometros. Sabiendo que millas equivale a 
+#1.609344 kilometros
+
+millas = 1.609344
+
+numero = float(input("Escribe un numero")) 
+
+kilometros = numero*millas
+
+print(numero," millas equivale a ",kilometros," kilometros")
