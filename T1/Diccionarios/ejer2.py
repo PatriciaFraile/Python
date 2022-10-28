af3d9cddbe52965c78131ff51eb441923cebd0fe
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2022-10-28 13:41:22 +0200

    subir

---
+++ T1/Diccionarios/ejer2.py
@@ -0,0 +1,18 @@
+precioadidas = 200
+precionike = 90
+preciopuma = 300
+preciototal = precioadidas+precionike+preciopuma
+
+zapatillas = {
+    'nombre1':'Adidas',
+    'precio1': precioadidas,
+    'nombre2': 'Nike',
+    'precio2': precionike,
+    'nombre3':'Puma',
+    'precio3':preciopuma,
+    'precio total':preciototal
+}
+print(zapatillas)
+
+
+
