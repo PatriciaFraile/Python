e13183f01fe854601c208900c1a026150dfdce7b
PatriciaFraile <93117363+PatriciaFraile@users.noreply.github.com>
2022-11-11 10:36:57 +0100

    subir

---
+++ T1/Diccionarios/ejer3.py
@@ -0,0 +1,11 @@
+'Añadir cosas en el diccionario de Harry Potter : apellido, edad , genero y padres'
+
+Diccionario = {
+    'Nombre' :'Harry',
+    'Apellido': 'Potter',
+    'Edad': 18,
+    'Género':'Masculino',
+    'Madre':'Lili',
+    'Padre':'James'
+}
+print(Diccionario)
