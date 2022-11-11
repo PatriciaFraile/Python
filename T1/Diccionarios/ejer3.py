99edce475e99c45402e3fd0dc760635cec3502bb
PatriciaFraile <93117363+PatriciaFraile@users.noreply.github.com>
2022-11-11 11:01:37 +0100

    subir

--- T1/Diccionarios/ejer3.py
+++ T1/Diccionarios/ejer3.py
@@ -6,7 +6,17 @@ Diccionario = {
     'edad': 18,
     'genero':'Masculino',
     'madre':'Lili',
-    'padre':'James'
+    'padre':'James',
+    'padres':['james','lili']
 }
 print(Diccionario)
 print(Diccionario['apellido'])
+print(Diccionario['padres'][0:1])#Sacar solamente un padre
+Diccionario['edad']=20 #cambiar el valor de una variable
+print(Diccionario['edad'])
+Diccionario['Grupo']=""#Añadir un nuevo elemento
+print(Diccionario)
+Diccionario["Casa"]= Diccionario.pop("Grupo") #Cambiar el nombre del elemento
+print(Diccionario)
+del Diccionario['apellido'] #Borra el elemento y su valor
+print(Diccionario)
