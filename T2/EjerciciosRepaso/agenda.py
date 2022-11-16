da48967f2fb93e7eccc81b728260a0eddcec5e9c
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2022-11-16 22:59:18 +0100

    subir

---
+++ T2/EjerciciosRepaso/agenda.py
@@ -0,0 +1,8 @@
+n = 0
+while 5 == n :
+    print("""1.Crear fichero
+       2. Añadir cliente
+       3.Consultar telefono
+       4.Borrar contacto
+       5. Salir""")
+    n = int(input("Elige una opción"))
