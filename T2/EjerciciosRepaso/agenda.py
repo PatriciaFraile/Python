ae31a5f5eb82dc333d18eb52bbe10380c6e2bdc8
PatriciaFraile <93117363+PatriciaFraile@users.noreply.github.com>
2022-11-17 12:14:28 +0100

    subir

--- T2/EjerciciosRepaso/agenda.py
+++ T2/EjerciciosRepaso/agenda.py
@@ -5,14 +5,22 @@ def menu():
    4.Borrar contacto
    5.Salir""")
 n = 0
+nombre_fichero = ""
 while n!=5:
    menu()
    n = int(input("Opcion"))
    if n == 1:
       print("Crear fichero")
-      #break
+      nombre_fichero = 'agenda'+'.txt'
+      print("Fichero creado ")
    elif n==2:
       print("Añadir contacto")
+      with open(nombre_fichero,'w')as f:
+         f.write(input("Añade contacto"))
+      f.close()
+   elif n==3:
+      print("Consultar agenda")
+
       #break
 print("salir")
 
