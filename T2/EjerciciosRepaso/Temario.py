9294b30f75048ee2e225a793486996f53009d981
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2022-11-24 20:40:17 +0100

    subir

---
+++ T2/EjerciciosRepaso/Temario.py
@@ -0,0 +1,6 @@
+listado = ["Tema 1: Introducción","Tema 2: Identificadores","Tema 3: Estructuras","Tema 4: Colecciones",
+"Tema 5: Orientación a objetos", "Tema 6: Clases y Objetos", "Tema 7; Abstracción, Herencia y Polimorfismo",
+ "Tema 8: Ficheros y Excepciones", "Tema 9: Bases de datos", "Tema 10: Interfaces"]
+
+for i in range(len(listado)):
+    print(listado[i])
--- T2/EjerciciosRepaso/mayorArray.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-numero = int(input("Longitud de arrays"))
-array = np.array([numero])
-print(len(array))
