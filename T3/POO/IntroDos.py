489f11ff652d8bfd70da46ab09c1660522a8081e
PatriciaFraile <93117363+PatriciaFraile@users.noreply.github.com>
2022-12-02 13:27:04 +0100

    subir

--- T3/POO/IntroDos.py
+++ T3/POO/IntroDos.py
@@ -1,1 +1,32 @@
 '''Objetos dentro de Objetos'''
+class Serie:
+    def __init__(self, titulo, captitulos, lanzamiento):
+        self.titulo = titulo
+        self.capitulos = captitulos
+        self.lanzamiento = lanzamiento
+        print('Se ha creado la serie:', self.titulo)
+
+    def __str__(self):
+        return '{} ({})'.format(self.titulo, self.lanzamiento)
+
+
+class Catalogo:
+
+    serie = []  
+
+    def __init__(self, serie=[]):
+        self.serie = serie
+
+    def agregar(self, p):  
+        self.serie.append(p)
+
+    def mostrar(self):
+        for p in self.serie:
+            print(p)  
+
+
+p = Serie("La que se avecina", 200, 2022)
+c = Catalogo([p])  
+c.mostrar()
+c.agregar(Serie("La que se avecina", 200, 2022))  
+c.mostrar()
