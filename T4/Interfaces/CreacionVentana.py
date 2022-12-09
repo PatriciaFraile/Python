778d01001877d5cf069d778e6306fe68edaebd1f
PatriciaFraile <93117363+PatriciaFraile@users.noreply.github.com>
2022-12-09 09:55:23 +0100

    subir

--- T4/Interfaces/CreacionVentana.py
+++ T4/Interfaces/CreacionVentana.py
@@ -1,5 +1,14 @@
 from tkinter import*
 raiz = Tk()
 raiz.title("Primera ventana")
+
+miFrame = Frame(raiz)#Es el marco de la ventana 
+miFrame.pack()
+miFrame.config(bg="green")#color de fondo
+miFrame.config(cursor="pirate")#cursor del ratón
+miFrame.config(width=480,height=320)
+
+label = Label(raiz,text='Hola')#Creacion de un label en la raiz
+label.pack()
 #Siempre el mainloop() es lo ultimo
 raiz.mainloop()
