03296bfe9628790769e697d0e0ccef4ff02131ba
PatriciaFraile <93117363+PatriciaFraile@users.noreply.github.com>
2022-12-09 10:22:08 +0100

    subir

--- T4/InterfacesGraficas/CreacionVentana.py
+++ T4/InterfacesGraficas/CreacionVentana.py
@@ -1,14 +1,16 @@
 from tkinter import*
 raiz = Tk()
 raiz.title("Primera ventana")
+raiz.config(bg="green")
+raiz.config(cursor="pirate")#Poner el cursor en toda la ventana 
 
 miFrame = Frame(raiz)#Es el marco de la ventana 
 miFrame.pack()
-miFrame.config(bg="green")#color de fondo
-miFrame.config(cursor="pirate")#cursor del ratón
+miFrame.config(bg="green")#color de fondo del frame
+'''miFrame.config(cursor="pirate")#cursor del ratón en el frame'''
 miFrame.config(width=480,height=320)
-
-label = Label(raiz,text='Hola')#Creacion de un label en la raiz
-label.pack()
+label = Label(raiz,text="Frame")#Creacion de un label en la raiz
+label.pack(anchor=CENTER)
+label.config(bg="green",fg="white")
 #Siempre el mainloop() es lo ultimo
 raiz.mainloop()
